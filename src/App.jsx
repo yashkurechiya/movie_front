8d765d359d7bae48eb262e4a341f683b1742c3fd
yashkurechiya <2005yashkurechiya@gmail.com>
2025-10-13 22:14:47 +0530

    remove compenents

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
-import Movies from './pages/Movies'
 import MovieDetails from './pages/MovieDetails'
-import SeatLayout from './pages/SeatLayout'
-import MyBookings from './pages/MyBookings'
-import Favorite from './pages/Favorite'
+ 
+ 
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
 import MoviesSection from './pages/MoviesSection'
@@ -39,12 +37,8 @@ const App = () => {
         <Route path='register' element={<Register />} />
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/movies' element={<Movies />} />
         <Route path='/videos/:id' element={<Trailer /> } />
         <Route path='/movies/:id' element={<MovieDetails />} />
-        <Route path='/movies/:id/:date' element={<SeatLayout />} />
-        <Route path='/my-bookings' element={<MyBookings />} />
-        <Route path='/favorite' element={<Favorite />} />
 
         {/* <Route path='' /> */}
       </Routes>
